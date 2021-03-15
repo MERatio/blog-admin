0e2a38169f75bd045c65a664b5e2d40bc1bcf95d
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-15 10:56:57 +0800

    Use BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootswatch/dist/litera/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
@@ -8,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Router basename={process.env.PUBLIC_URL}>
+			<App />
+		</Router>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
