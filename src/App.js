647bc824740a7ce0ed269b1c3d1b644d0709616d
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-15 14:09:30 +0800

    Add sign in functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Bus from './utils/Bus';
 import Flashes from './components/Flashes';
 import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
 import './App.css';
 
 function App() {
+	const [user, setUser] = useState(null);
+
 	useEffect(() => {
 		window.flashes = (flashes) => Bus.emit('flashes', flashes);
 	}, []);
@@ -26,6 +29,15 @@ function App() {
 							<div className="row justify-content-center">
 								<div className="col-md-6">
 									<SignUpForm />
+								</div>
+							</div>
+						</div>
+					</Route>
+					<Route exact path="/sign-in">
+						<div className="container">
+							<div className="row justify-content-center">
+								<div className="col-md-6">
+									<SignInForm setUser={setUser} />
 								</div>
 							</div>
 						</div>
---
+++ src/components/SignInForm.js
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import useIsMounted from '../lib/useIsMounted';
+import useIsLoading from '../lib/useIsLoading';
+import { postData, handleExpressErr } from '../lib/helpers';
+import SubmitBtn from './SubmitBtn';
+
+function SignInForm({ setUser }) {
+	const history = useHistory();
+
+	const isMounted = useIsMounted();
+	const [submitFormData, isSubmitting] = useIsLoading(postData);
+
+	const [state, setState] = useState({
+		username: '',
+		password: '',
+	});
+
+	function handleInputChange(e) {
+		const target = e.target;
+		const name = target.name;
+		const value = target.value;
+		setState((prevState) => ({ ...prevState, [name]: value }));
+	}
+
+	async function handleSubmit(e) {
+		try {
+			e.preventDefault();
+			const data = await submitFormData(
+				`${process.env.REACT_APP_API_URL}/auth/sign-in`,
+				state
+			);
+			if (data.err) {
+				isMounted && setState((prevState) => ({ ...prevState, password: '' }));
+				handleExpressErr(data.err);
+			} else {
+				if (data.user && data.user.admin) {
+					localStorage.setItem('jwt', data.token);
+					setUser(data.user);
+					window.flashes([
+						{ msg: 'You have successfuly signed in', type: 'success' },
+					]);
+					history.push('/');
+				} else {
+					isMounted && setState({ username: '', password: '' });
+					window.flashes([
+						{ msg: `Admin account is required`, type: 'warning' },
+					]);
+				}
+			}
+		} catch (err) {
+			window.flashes([
+				{ msg: 'Something went wrong, please try again later.' },
+			]);
+		}
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className="form-group">
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					className="form-control"
+					id="username"
+					placeholder="johnDoe01"
+					name="username"
+					value={state.username}
+					required
+					maxLength="20"
+					onChange={handleInputChange}
+				/>
+			</div>
+			<div className="form-group">
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					className="form-control"
+					id="password"
+					name="password"
+					value={state.password}
+					required
+					minLength="8"
+					onChange={handleInputChange}
+				/>
+			</div>
+			<SubmitBtn isSubmitting={isSubmitting} />
+		</form>
+	);
+}
+
+SignInForm.propTypes = {
+	setUser: PropTypes.func.isRequired,
+};
+
+export default SignInForm;
