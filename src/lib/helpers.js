2eda80f113ebbb2bcaf4d383c91ff874a916d531
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-15 13:11:26 +0800

    Add postData helper

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,16 @@
+async function postData(url = '', data = {}) {
+	const response = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+	return response.json();
+}
+
 function handleExpressErr(err) {
 	window.flashes([{ msg: err.message }]);
 }
 
-export { handleExpressErr };
+export { postData, handleExpressErr };
