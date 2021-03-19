852883c8084956a0dbeccedff2dc268da8b9f1c0
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-19 18:48:05 +0800

    Reset published after errors or successful submit

--- src/pages/NewPostPage.js
+++ src/pages/NewPostPage.js
@@ -33,11 +33,11 @@ function NewPostPage() {
 			if (data.err) {
 				handleExpressErr(data.err);
 			} else if (data.errors) {
-				const { title, body } = data.post;
-				isMounted && setState({ title, body });
+				const { title, body, published } = data.post;
+				isMounted && setState({ title, body, published });
 				window.flashes(data.errors);
 			} else {
-				setState({ title: '', body: '' });
+				setState({ title: '', body: '', published: false });
 				window.flashes([{ msg: 'Post successfully created', type: 'success' }]);
 			}
 		} catch (err) {
