7591a4a991a8f33bf0e7a0c9840e7a22135a9634
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-27 13:06:13 +0800

    Switch BrowserRouter (and and delete its basename attr) to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import 'bootswatch/dist/litera/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
@@ -12,7 +12,7 @@ dotenv.config();
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Router basename={process.env.PUBLIC_URL}>
+		<Router>
 			<App />
 		</Router>
 	</React.StrictMode>,
